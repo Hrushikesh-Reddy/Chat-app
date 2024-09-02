e922f6945992ecbec7b7a989879a0b3c75dd5d5d
Hrushikesh <hrushikesh1330@gmail.com>
2024-09-02 16:34:57 +0530

    minor changes

--- frontend/src/scripts/socket.js
+++ frontend/src/scripts/socket.js
@@ -1,9 +1,9 @@
 import { io } from "socket.io-client";
 
 const URL =
-"/choreo-apis/chatapp/backend/v1"
+  "https://c37ebc4c-8f89-49f7-bc20-30c8c34ad918-dev.e1-us-east-azure.choreoapis.dev/chatapp/backend/v1.0";
 
-export const socket = io(undefined, {
+export const socket = io(URL, {
   //transports: ["websocket"],
   autoConnect: false,
 });
