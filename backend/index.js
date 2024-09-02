d72a8b78defb229f353ab46b0d953e8d0fe34f84
Hrushikesh <hrushikesh1330@gmail.com>
2024-09-02 16:06:28 +0530

    minor changes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
+//const cors = require("cors");
 const { createServer } = require("node:http");
 const { Server } = require("socket.io");
 const { saveMessage } = require("./Model/database");
@@ -20,7 +20,7 @@ const chatRouter = require("./Routes/chatRouter");
 
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors());
+//app.use(cors());
 
 app.use("/api", authRouter);
 app.use("/chat", chatRouter);
@@ -33,9 +33,9 @@ async function gen(prompt) {
 }
 
 const io = new Server(httpServer, {
-  cors: {
+/*   cors: {
     origin: "/choreo-apis/chatapp/backend/v1",
-  },
+  }, */
   connectionStateRecovery: {},
 });
 
