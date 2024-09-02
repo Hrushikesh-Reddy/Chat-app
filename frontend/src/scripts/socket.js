f63ae663294f34106dbb8fd739418609808cf1b0
Hrushikesh <hrushikesh1330@gmail.com>
2024-09-02 18:17:08 +0530

    minor changes

--- frontend/src/scripts/socket.js
+++ frontend/src/scripts/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "https://d375fde1-518f-48b0-a740-d2e359b482eb.e1-us-east-azure.choreoapps.dev/choreo-apis/chatapp/backend/v1";
+const URL = "https://chat-app-c2ux.onrender.com/";
 
 export const socket = io(URL, {
   //transports: ["websocket"],
