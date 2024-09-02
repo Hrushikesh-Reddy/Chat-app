9c5e635c0a78d3bf234a6c489ab42309c947d9e4
Hrushikesh <hrushikesh1330@gmail.com>
2024-09-02 13:40:56 +0530

    minor changes

--- frontend/src/scripts/socket.js
+++ frontend/src/scripts/socket.js
@@ -1,5 +1,5 @@
 import {io} from 'socket.io-client'
 
-const URL = "/choreo-apis/chatapp/backend/v1"
+const URL = "https://d375fde1-518f-48b0-a740-d2e359b482eb.e1-us-east-azure.choreoapps.dev/choreo-apis/chatapp/backend/v1"
 
 export const socket = io(URL, {autoConnect:false}) 
