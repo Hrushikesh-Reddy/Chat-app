425f2db2db64e7f7c17af79aab7de506c787201a
Hrushikesh <hrushikesh1330@gmail.com>
2024-09-02 14:52:45 +0530

    minor changes

--- frontend/src/Components/RoomContent.jsx
+++ frontend/src/Components/RoomContent.jsx
@@ -77,13 +77,13 @@ export default function RoomContent({
 
     function onConnectError(err) {
       // the reason of the error, for example "xhr poll error"
-      console.log(err.message);
+      console.log("the reason of the error", err.message);
 
       // some additional description, for example the status code of the initial HTTP response
-      console.log(err.description);
+      console.log("some additional description : ", err.description);
 
       // some additional context, for example the XMLHttpRequest object
-      console.log(err.context);
+      console.log("some additional context,", err.context);
     }
 
     socket.on("connect", onConnect);
