166d66085f9a13ee53af69a2d45b27e9d86bda41
Hrushikesh <hrushikesh1330@gmail.com>
2024-09-03 20:27:20 +0530

    ...

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -3,6 +3,7 @@ import Form from "../Components/Form";
 export default function Login() {
   return (
     <>
+    <p>[Test :- Username : user1, Password: password1]</p>
       <Form method="login" route="/api/login" />
     </>
   );
