6ba6aceae90a45f04d798dec9093e279deaa093d
Hrushikesh <hrushikesh1330@gmail.com>
2024-09-07 18:51:01 +0530

    minor changes

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,13 @@ app.use(cors({
 }
 ));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Auth");
+  next();
+});
+
 app.use("/api", authRouter);
 app.use("/chat", chatRouter);
 app.use(genRouter);
