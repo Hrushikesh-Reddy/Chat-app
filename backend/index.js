d7145a74a088a129153820fa32c5787423c8a7ed
Hrushikesh <hrushikesh1330@gmail.com>
2024-09-14 12:30:42 +0530

    minor changes

--- backend/index.js
+++ backend/index.js
@@ -20,12 +20,13 @@ const chatRouter = require("./Routes/chatRouter");
 
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(function(req, res, next) {
+/*  app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Auth, auth");
   next();
-});
+}); */
+
 app.use(cors());
 
 
--- frontend/src/scripts/api.js
+++ frontend/src/scripts/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiURL = "https://c37ebc4c-8f89-49f7-bc20-30c8c34ad918-prod.e1-us-east-azure.choreoapis.dev/backend/chatapp/v1.0";
+//const apiURL = "https://c37ebc4c-8f89-49f7-bc20-30c8c34ad918-prod.e1-us-east-azure.choreoapis.dev/backend/chatapp/v1.0/";
+//const apiURL = "http://localhost:3000/";
+const apiURL = "https://chat-app-c2ux.onrender.com/";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiURL,
